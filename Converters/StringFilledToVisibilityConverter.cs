f6d97f78e8251bbde099ebeaf371023bad01023a
jamied <hey@isuckat.dev>
2025-10-03 16:34:25 -0400

    empty lyrics on itunes tracks now display correctly

---
+++ Converters/StringFilledToVisibilityConverter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Diagnostics;
+
+namespace Musium.Converters
+{
+    public class StringFilledToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string s)
+            {
+                bool isFilled = !string.IsNullOrEmpty(s) || !string.IsNullOrWhiteSpace(s);
+                
+                if (parameter is string param && param.Equals("Inverse", StringComparison.OrdinalIgnoreCase))
+                {
+                    isFilled = !isFilled;
+                }
+                if (isFilled) Debug.WriteLine(s + " - should be lyrics before");
+
+                return isFilled ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            if (parameter is string par && par.Equals("Inverse", StringComparison.OrdinalIgnoreCase)) return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value is Visibility visibility && visibility == Visibility.Visible;
+        }
+    }
+}
+
+    
